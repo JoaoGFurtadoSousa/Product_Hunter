7da3cdca17a0328c1df5f833290fd1de30f6b039
JoaoG <165217153+JoaoGFurtadoSousa@users.noreply.github.com>
2026-02-26 14:05:07 -0300

    feat(Applications): Metodo Post criado

--- applications/models.py
+++ applications/models.py
@@ -5,7 +5,7 @@ from hashtags.models import Hashtags
 class APP(models.Model):
     name = models.CharField(max_length= 60)
     description = models.CharField(max_length= 300)
-    likes = models.IntegerField()
+    likes = models.IntegerField(default= 0)
     logo_app = models.ImageField(upload_to='media')
     hashtags = models.ManyToManyField(Hashtags)
     
--- applications/serializers.py
+++ applications/serializers.py
@@ -5,4 +5,4 @@ from .models import APP
 class APPSerializer(serializers.ModelSerializer):
     class Meta:
         model = APP
-        fields = '__all__'
+        fields = ('name', 'description', 'hashtags',)
