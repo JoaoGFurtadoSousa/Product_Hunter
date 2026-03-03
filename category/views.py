c07db88f7d6ef2f87d0e65d8ef767925c56e19ad
JoaoG <165217153+JoaoGFurtadoSousa@users.noreply.github.com>
2026-03-02 23:23:17 -0300

    feat(CAtegory): Endpoint para GET de Category implementado

--- category/views.py
+++ category/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
+from rest_framework.views import APIView
+from .models import Category
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from .serializers import CategorySerializer
 # Create your views here.
+
+class CategoryView(APIView):
+    permission_classes = [IsAuthenticated, ]
+
+    def get(self, request):
+        categorys = Category.objects.all()
+        print(categorys)
+        return Response(CategorySerializer(categorys, many= True).data)
--- core/urls.py
+++ core/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/v1/', include('applications.urls')),
+    path('api/v1/', include('category.urls')),
 ]
