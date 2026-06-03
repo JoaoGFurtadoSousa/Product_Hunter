0448bd17334665a75f0921b9de694e3a2b18293e
JoaoGFS <165217153+JoaoGFurtadoSousa@users.noreply.github.com>
2026-06-03 10:22:36 -0300

    feat(applications): Utiliazando langchain para trabalhar com cache prompts

--- applications/integration_ia.py
+++ applications/integration_ia.py
@@ -1,8 +1,22 @@
-# from google import genai
-# from decouple import config
+from langchain_google_genai import GoogleGenerativeAI
+from langchain_core.globals import set_llm_cache
+from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
+from langchain_community.cache import SQLiteCache
+from decouple import config
 
 
-# client = genai.Client(api_key = config('GEMINI_API_KEY'))
+
+set_llm_cache(SQLiteCache(database_path=config('NAME_DB_CACHE_LANGCHAIN')))
+
+model = GoogleGenerativeAI(model= "gemini-3.1-flash-lite",
+                           api_key=config('GEMINI_API_KEY'),
+                           )
+
+prompt = "Me fale sobre django"
+
+response = model.invoke(prompt)
+print(response)
+
 
 # def details_the_app_description(description_app: str):
 #     response = client.models.generate_content(
